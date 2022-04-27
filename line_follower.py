d32f9547dd32de4f1647980905925d0f9ee77ff2
jestem-andrew <jedrekmalolepszy@gmail.com>
2022-04-28 00:21:37 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! debugowanie

--- line_follower.py
+++ line_follower.py
@@ -1,24 +1,25 @@
 from gamepad_control import GamepadControl
-#from motor_driver import MotorDriver
+from motor_driver import MotorDriver
 #import RPi.GPIO as GPIO
 
 #from inputs import get_gamepad
 #from inputs import devices
 
 
-class LineFollower(GamepadControl):
+class LineFollower(GamepadControl, MotorDriver):
   """Main class used for controlling the line follower.
   """
   def useGamePad(self):
     print("gowno")
     dir = self.getGamepadDirection()
     print(dir)
-    #MotorDriver.goDirection(dir)
+    self.goDirection(dir)
 
 
 def main():
   lineFollower = LineFollower()
-  lineFollower.useGamePad()
+  while(1):
+    lineFollower.useGamePad()
 
 if __name__ == "__main__":
   main()
