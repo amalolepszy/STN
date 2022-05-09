7e5b69750657d429da401caed8b3d7971a4f1cb7
jestem-andrew <jedrekmalolepszy@gmail.com>
2022-05-09 17:34:10 +0200

    fixup! Added stand still gamepad option

--- gamepad_control.py
+++ gamepad_control.py
@@ -19,7 +19,7 @@ class GamepadControl():
       for event in events:
         print(event.ev_type, event.code, event.state)
       
-  def getGamepadDirection(self, sensitivity = 15000):
+  def getGamepadDirection(self, sensitivity = 10000):
     dir = ""
     events = self._getGamepadEvent()
     for event in events:
