29895c5da3f9b47514a9853e840f9914d0e2e9e5
jestem-andrew <jedrekmalolepszy@gmail.com>
2022-05-23 20:10:09 +0200

    Fix linter issues

--- line_follower.py
+++ line_follower.py
@@ -1,8 +1,6 @@
+import argparse
 from src.gamepad_control import GamepadControl
 from src.motor_driver import MotorDriver
-
-import argparse
-
 
 class LineFollower(GamepadControl, MotorDriver):
   """Main class used for controlling the line follower.
@@ -10,15 +8,14 @@ class LineFollower(GamepadControl, MotorDriver):
   def useGamePad(self):
     self.goDirection(self.getGamepadDirection())
 
-
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('use_gamepad')
   args = parser.parse_args()
   lineFollower = LineFollower()
   lineFollower.initMotorPins()
-  if(args.use_gamepad == 'use_gamepad'):
-    while(1):
+  if args.use_gamepad == 'use_gamepad':
+    while True:
       lineFollower.useGamePad()
 
 if __name__ == "__main__":
