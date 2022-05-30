992d7700287d7b475d091b4a952e4d9b5a88bd8c
jestem-andrew <jedrekmalolepszy@gmail.com>
2022-05-30 15:35:34 +0200

    Play sound when pressing A button

--- src/line_follower.py
+++ src/line_follower.py
@@ -1,12 +1,17 @@
 import argparse
 from gamepad_control import GamepadControl
 from motor_driver import MotorDriver
+from sound_signals import Sounds
 
-class LineFollower(GamepadControl, MotorDriver):
+class LineFollower(GamepadControl, MotorDriver, Sounds):
   """Main class used for controlling the line follower.
   """
   def useGamePad(self):
     self.goDirection(self.getGamepadDirection())
+  
+  def playSounds(self):
+    if(self.getPressedButton == "A"):
+      self.playUwazaj()
 
 def main():
   parser = argparse.ArgumentParser()
@@ -17,6 +22,7 @@ def main():
   if args.use_gamepad == 'use_gamepad':
     while True:
       lineFollower.useGamePad()
+      lineFollower.playSounds()
 
 if __name__ == "__main__":
   main()
