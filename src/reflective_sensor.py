efe9627ea58c5bf0994978bd448e2dc4c41f04f3
jestem-andrew <jedrekmalolepszy@gmail.com>
2022-06-09 13:32:00 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Changing in reflective

--- src/reflective_sensor.py
+++ src/reflective_sensor.py
@@ -88,7 +88,7 @@ class ReflectiveSensor(mDriver):
   def ride_reflective(self):
     while(1):
       print("LEFT_FAR " + str(GPIO.input(sPins.LEFT_FAR)) +"\nLEFT_FAR " + str(GPIO.input(sPins.LEFT_CLOSE)) + "\nCENTER " + str(GPIO.input(sPins.CENTER)) + "\nRIGHT_CLOSE " + str(GPIO.input(sPins.RIGHT_CLOSE)) + "\nRIGHT_FAR " + str(GPIO.input(sPins.RIGHT_FAR)))
-      if (not (GPIO.input(sPins.RIGHT_FAR) and GPIO.input(sPins.LEFT_CLOSE) and GPIO.input(sPins.CENTER) and GPIO.input(sPins.LEFT_CLOSE) and GPIO.input(sPins.LEFT_FAR))):
+      if (((GPIO.input(sPins.RIGHT_FAR) and GPIO.input(sPins.LEFT_CLOSE) and GPIO.input(sPins.CENTER) and GPIO.input(sPins.LEFT_CLOSE) and GPIO.input(sPins.LEFT_FAR)))):
         self._standStill()
       if(GPIO.input(sPins.LEFT_FAR) == True):
         self._goLeft()
